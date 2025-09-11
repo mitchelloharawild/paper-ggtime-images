3b727a455bde186e1e709951aa1491fb9b7f9df6
Mitchell O'Hara-Wild <mail@mitchelloharawild.com>
2025-09-12 00:11:16 +1000

    Better labels for ACF

--- src/paper-figures-code/beer-acf.R
+++ src/paper-figures-code/beer-acf.R
@@ -2,5 +2,6 @@ source("src/paper-figures-code/setup-common.R")
 p <- aus_production |> 
   filter(year(Quarter) >= 1991) |>
   feasts::ACF(Beer) |> 
-  autoplot()
+  autoplot() + 
+  labs(x = "Lag (Quarters)", y = "Autocorrelation (ACF)")
 ggsave("plots/plot-acf.png", plot = p, width = 12, height = 8, units = "cm")
